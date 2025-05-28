d74004b1724cc7bddd0fb4af9b6e9efd3046d12f
drupchen <hhdrupchen@gmail.com>
2025-05-28 20:41:22 +0100

    update to new workflow

--- process_recordings/chunk_recordings.py
+++ process_recordings/chunk_recordings.py
@@ -13,48 +13,108 @@ def parse_catalog(catalog):
         return millis
 
     parsed = defaultdict(list)
+    # initial parse + reformat timecodes to ms
     with open(catalog, newline='') as csvfile:
-        rows = list(csv.reader(csvfile, delimiter='\t', quotechar='|'))
-        legend, rows = rows[0], rows[1:]
-        table = {l: n for n, l in enumerate(legend)}
-        for r in rows:
-            start = to_milliseconds(time.fromisoformat(r[table['start']]))
-            duration = to_milliseconds(time.fromisoformat(r[table['duration']]))
-            entry = {'start': start,
-                     'duration': duration,
-                     'filename': r[table['export filename']],
-                     'filename_session': r[table['session filename']],
-                     'session_title': r[table['session title']]
-                     }
-            parsed[r[table['recording filename']]].append(entry)
-    return parsed
+        cat = csv.DictReader(csvfile, delimiter='\t', quotechar='|')
+        for row in cat:
+            file = row['filename'][:row['filename'].rfind('.')]
+            if row['start']:
+                row['start'] = to_milliseconds(time.fromisoformat(row['start']))
+            else:
+                row['start'] = None
+            if row['end']:
+                row['end'] = to_milliseconds(time.fromisoformat(row['end']))
+            else:
+                row['end'] = None
+            if row['duration']:
+                row['duration'] = to_milliseconds(time.fromisoformat(row['duration']))
+            else:
+                row['duration'] = None
+            parsed[file].append(row)
+
+    # group in teaching sessions
+    processed_in_sessions = {}
+    S, S_T = 'session number', 'translation session number'
+    for filename, parts in parsed.items():
+        # filter out all unprocessed files
+        if not parts[0][S] and not parts[0][S_T]:
+            continue
+
+        sessions = {}
+        for p in parts:
+                # 1. parse main teaching
+                session, part = None, None
+                if ',' in p[S]:
+                    session, part = p[S].split(',')
+                else:
+                    session, part = p[S], '1'
+
+                if session:
+                    # create scaffolding
+                    if session not in sessions:
+                        sessions[session] = []
+
+                    sessions[session].append((part, p))
+
+                # 2. parse translation
+                session_trans, part_trans = None, None
+                if ',' in p[S_T]:
+                    session_trans, part_trans = p[S_T].split(',')
+                else:
+                    session_trans, part_trans = p[S_T], '1'
+
+                if session_trans:
+                    session_trans += '_trans'
+                    if session_trans not in sessions:
+                        sessions[session_trans] = []
+
+                    sessions[session_trans].append((part_trans, p))
+        processed_in_sessions[filename] = sessions
+
+    return parsed, processed_in_sessions
 
 
-def export_sessions(catalog, audio_path, out_path, pass_missing=False, single_file=''):
+def export_sessions(catalog, audio_path, out_path, pass_missing=False, single_file='', final_filename=False):
     out_path.mkdir(exist_ok=True, parents=True)
     for audio_file, sessions in catalog.items():
-        if single_file and audio_file != single_file:
-            continue
         print(audio_file)
-        af = audio_path / f'{audio_file}.mp3'
+        if '0' in sessions:
+            af = audio_path / sessions['0'][0][1]['filename']
+        elif '1' in sessions:
+            af = audio_path / sessions['1'][0][1]['filename']
+        else:
+            print('This should not happen. each session should either start with 0 or 1. Exiting')
+            exit(0)
         if not af.is_file():
             if pass_missing:
-                print('not parsing', af)
+                print('not parsing. file missing in input folder:', af)
                 continue
             else:
                 raise FileExistsError(af)
 
         audio = AudioSegment.from_file(af)
-        for s in sessions:
-            print('\t', s['filename'])
-            out_file = out_path / f'{s["filename"]}.mp3'
-            session = audio[s['start']:s['start']+s['duration']]
-            session.export(out_file, format="mp3")
+        for s_name, s in sessions.items():
+            if final_filename:
+                filename = s[0][1]['export filename']
+            else:
+                ext = s[0][1]['filename'][s[0][1]['filename'].rfind('.')+1:]
+                filename = f'{audio_file}_{s_name}.{ext}'
+            print('\t', filename)
+
+            session_audio = AudioSegment.empty()
+            for part_num, part in s:
+                start, duration = part['start'], part['duration']
+                audio_part = audio[start:start+duration]
+                session_audio += audio_part
+            out_folder = out_path / part['Folder'] / audio_file
+            out_folder.mkdir(parents=True, exist_ok=True)
+            out_file = out_folder / filename
+            session_audio.export(out_file, format="wav")
 
 
 def export_teachings(catalog, audio_path, out_path, pass_missing=False, single_file=''):
-    catalog = parse_catalog(catalog)
-    export_sessions(catalog, audio_path, out_path, pass_missing=pass_missing, single_file=single_file)
+    catalog, catalog_sessions = parse_catalog(catalog)
+    export_sessions(catalog_sessions, audio_path, out_path, pass_missing=pass_missing, single_file=single_file)
 
 
 def export_final_files(catalog_path, mp3_path, srt_path, out):
--- usage.py
+++ usage.py
@@ -4,19 +4,19 @@ from urllib.request import urlretrieve
 from process_recordings import export_teachings, export_final_files
 
 # modes:
-# 1: export individual sessions from the cassette sides + resegment sessions when needed
-# 2: when everything is aligned, export final sessions + .srt files for hyperaudio with final filenames + tibetan session title
-mode = 2
+# 1. Segmentation process: export individual sessions from the cassette sides + resegment sessions when needed
+# 2. Alignment process: when everything is aligned, export final sessions + .srt files for hyperaudio with final filenames + tibetan session title
+mode = 1
 
 if mode == 1:
     # download from Google Drive
-    catalog_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSGZJWL9nTtAurpNBs3AVK7UHELhEW41I3t1u9NZHU8xDxPKHTz9Qml1W_jVJ4vpyQzZxDDLv6Q2pq2/pub?gid=708229619&single=true&output=tsv'
-    filename = "input/tsiksum nedek catalog - final catalog.tsv"
+    catalog_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5UhciIy-L82-xPJJ5TmpNS-Hizae9ot_2fuvbl2cPQvuBwwRYfpYSSQkeSletINUeaq3UILKlP0gA/pub?gid=2043758745&single=true&output=tsv'
+    filename = "input/audio archives - sessions.tsv"
     urlretrieve(catalog_url, filename)
 
-    audio_path = Path('input/mp3_tsiksum_nedek/')
-    out_path = Path('output/tsiksum nedek')
-    cassette_side_to_resegment = 'Tsig Sum Nedek 1B'
+    audio_path = Path('/media/drupchen/Khyentse Önang/Khyentse Önang/Processing')
+    out_path = Path('output/Raw Sessions')
+    cassette_side_to_resegment = ''
     export_teachings(Path(filename), audio_path, out_path, pass_missing=True, single_file=cassette_side_to_resegment)
 
 elif mode == 2:
